c3c49b31ce8bd38d4109a5f14483da977913a566
raphael.banquetti@lamanu-student.fr <raphael.banquetti@lamanu-student.fr>
2026-01-23 10:20:17 +0100

    first commit

---
+++ assets/js/pageAccueil.js
@@ -0,0 +1,37 @@
+// =================HEADER=================
+const body = document.getElementById("body")
+
+const header = document.createElement("header")
+const titre = document.createElement('h1')
+
+titre.classList.add('titre')
+
+titre.textContent = "Mon portfolio"
+
+header.append(titre)
+body.appendChild(header)
+
+// =================LIENS=================
+const main = document.createElement('main')
+const liste = document.createElement('li')
+const navLiens1 = document.createElement('ul')
+const navLiens2 = document.createElement('ul')
+const liens1 = document.createElement('a')
+const liens2 = document.createElement('a')
+
+liens1.setAttribute('href', './pageWebDev.html')
+liens1.setAttribute('target', '_blank')
+
+liens2.setAttribute('href', './pageDesign.html')
+liens2.setAttribute('target', '_blank')
+
+liste.classList.add('menu')
+
+liens1.textContent = 'Projets WEB/Développement'
+liens2.textContent = 'Projets DESIGN'
+
+navLiens1.appendChild(liens1)
+navLiens2.appendChild(liens2)
+liste.append(navLiens1, navLiens2)
+main.append(liste)
+body.append(main)
---
+++ assets/js/pageWeb.js
@@ -0,0 +1,94 @@
+const projets = [
+    {
+        nom: 'Star Wars',
+        class: 'star-wars',
+        github: 'https://github.com/h-raph/star-wars-trilogie.git',
+        site: 'https://star-wars-trilogie.vercel.app/',
+        image: '../assets/images/Star-wars.png',
+        langage: 'HTML & CSS',
+        description: "J'avais pour exercice de cibler les bons sélecteurs dans le fichier css",
+    },
+    {
+        nom: 'Collectif Rainbow Photo',
+        class: 'collectif-rainbow-photo',
+        github: 'https://github.com/h-raph/Collectif-Rainbow-Photo.git',
+        site: 'https://collectif-rainbow-photo.vercel.app/',
+        image: '../assets/images/Rainbow.png',
+        langage: 'HTML & CSS',
+        description: "Je devais styliser la page d'accueil",
+    },
+    {
+        nom: 'Site mariage',
+        class: 'site-mariage',
+        github: 'https://github.com/h-raph/Site-mariage.git',
+        site: 'http://127.0.0.1:5501/CSS/Exercices/TP%205/Mariage/enonce.html',
+        image: '../assets/images/Mariage.png',
+        langage: 'HTML & CSS',
+        description: "Je devais styliser la page d'accueil"
+    },
+    {
+        nom: 'Bibliothèque',
+        class: 'bibliotheque',
+        github: 'https://github.com/h-raph/Bibliotheque',
+        site: 'https://bibliotheque-peach.vercel.app/',
+        image : '../assets/images/Bibliotheque.png',
+        langage: 'HTML, CSS & JS',
+        description: "Je devais écrire la page HTML mais dans le fichier JavaScript"
+    }
+]
+
+// =================HEADER=================
+const body = document.getElementById("body")
+
+const header = document.createElement("header")
+const titre = document.createElement('h1')
+
+titre.classList.add('titre')
+
+titre.textContent = "Mes projets"
+
+header.append(titre)
+body.appendChild(header)
+
+// =================SECTION=================
+const section = document.createElement("section")
+
+section.setAttribute("id", "projets")
+
+body.append(section)
+
+// =================PROJETS=================
+for (let i = 0; i < projets.length; i++) {
+    const cardsProjets = document.createElement('div')
+    const boutonDiv = document.createElement('div')
+    const imageProjets = document.createElement('img')
+    const nomProjets = document.createElement('h2')
+    const langage = document.createElement('h3')
+    const description = document.createElement('p')
+    const boutonGitHub = document.createElement('button')
+    const boutonSite = document.createElement('button')
+    
+    cardsProjets.classList.add(`cards`, "projets-" + projets[i].class)
+    boutonGitHub.classList.add('github')
+    boutonGitHub.classList.add('btn')
+    boutonSite.classList.add('site')
+    boutonSite.classList.add('btn')
+    boutonDiv.classList.add('bouton-git-site')
+
+    imageProjets.src = projets[i].image
+    imageProjets.setAttribute('alt', projets[i].nom)
+
+    boutonGitHub.setAttribute('onclick', `window.open('${projets[i].github}')`)
+    boutonSite.setAttribute('onclick', `window.open('${projets[i].site}')`)
+
+    langage.textContent = projets[i].langage
+    nomProjets.textContent = projets[i].nom
+    description.textContent = projets[i].description
+
+    boutonGitHub.append('GitHub')
+    boutonSite.append('Voir la page')
+    boutonDiv.append(boutonSite, boutonGitHub)
+    cardsProjets.append(imageProjets, nomProjets, langage, description, boutonDiv)
+
+    section.append(cardsProjets)
+}
